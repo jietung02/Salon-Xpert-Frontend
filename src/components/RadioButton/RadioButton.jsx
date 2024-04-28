ebb4a464848be372517625769b9bc2216c7dcd8f
01 Jieddie <jietung02>
2024-04-28 15:29:24 +0800

    fixed radio button not responsive in mobile

--- src/components/RadioButton/RadioButton.jsx
+++ src/components/RadioButton/RadioButton.jsx
@@ -3,7 +3,7 @@ import RadioButtonOption from "./RadioButtonOption";
 export default function RadioButton({ bookingMethod, label, name, handleOnChange, options, type }) {
 
   return (
-    <div className={`relative w-full flex flex-wrap mx-auto ${type && type === 'scale'? ' lg:px-32 2xl:px-40': ''} px-0 text-center ${type && type === 'category' ? 'mt-8': ''}`}>
+    <div className={`relative w-full flex flex-wrap mx-auto ${type && type === 'scale'? 'xl:px-24 2xl:px-40': ''} px-0 text-center ${type && type === 'category' ? 'mt-8': ''}`}>
       <span className="text-gray-500 text-lg 2xl:text-xl w-full py-3 mb-2 ">{label}</span>
 
       {options.length > 0 && options.map((value) => (
